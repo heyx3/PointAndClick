3eeb45dc0d347a35ad841f46bc00404220a24aa1
heyx3 <manning.w@husky.neu.edu>
2014-09-16 12:49:13 -0400

    Committed initial Unity project

---
+++ Unity project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Controls the player.
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+	public GameObject IdleAnim, WalkLeftAnim, WalkRightAnim;
+	public GameObject CurrentlyActiveAnim = null;
+
+
+	public void SwitchToIdleAnim()
+	{
+		IdleAnim.SetActive(true);
+		WalkLeftAnim.SetActive(false);
+		WalkRightAnim.SetActive(false);
+		CurrentlyActiveAnim = IdleAnim;
+	}
+	public void SwitchToWalkLeftAnim()
+	{
+		IdleAnim.SetActive(false);
+		WalkLeftAnim.SetActive(true);
+		WalkRightAnim.SetActive(false);
+		CurrentlyActiveAnim = WalkLeftAnim;
+	}
+	public void SwitchToWalkRightAnim()
+	{
+		IdleAnim.SetActive(false);
+		WalkLeftAnim.SetActive(false);
+		WalkRightAnim.SetActive(true);
+		CurrentlyActiveAnim = WalkRightAnim;
+	}
+
+
+	void Awake()
+	{
+		SwitchToIdleAnim();
+	}
+
+	void Update()
+	{
+		
+	}
+}
