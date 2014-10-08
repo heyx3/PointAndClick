bd62b1693b85db67e90e9f4e22115ffc72d9be85
stuarrt <ramgolam.s@husky.neu.edu>
2014-10-08 14:09:53 -0400

    Changed Ambient Lighting script to follow player

--- Unity project/Assets/Scripts/AmbientLighting.cs
+++ Unity project/Assets/Scripts/AmbientLighting.cs
@@ -11,16 +11,22 @@ public class AmbientLighting : MonoBehaviour
 	public float NormalBrightness = 0.4f,
 				 UsingFlashlightBrightness = 0.2f;
 
-
+	public Transform MyTransform { get; private set; }
 	public Camera MyCamera { get; private set; }
 
 
 	void Awake()
 	{
+		MyTransform = transform;
 		MyCamera = camera;
 	}
 	void Update()
 	{
+		Vector3 pos = MyTransform.position;
+		Vector3 playerPos = PlayerInputController.Instance.MyTransform.position;
+
+		MyTransform.position = new Vector3(playerPos.x, pos.y, pos.z);
+
 		if (PlayerInputController.Instance.IsUsingFlashlight)
 		{
 			MyCamera.backgroundColor = new Color(UsingFlashlightBrightness,
